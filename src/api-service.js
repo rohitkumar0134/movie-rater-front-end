dce1346dd28ec99f219ea832b9d1f688e2f4438f
rohitkumar0134 <rohit54pandit@gmail.com>
2023-03-16 11:08:53 +0530

    fihal work kr rha h

--- src/api-service.js
+++ src/api-service.js
@@ -1,71 +1,59 @@
-export class API{
+export class API {
+  static loginUser(body) {
+    return fetch(`https://course-django-react-api-production.up.railway.app/auth/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify( body )
+    }).then( resp => resp.json())
+  }
+  static registerUser(body) {
+    return fetch(`https://course-django-react-api-production.up.railway.app/api/users/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify( body )
+    }).then( resp => resp.json())
+  }
 
-    static loginUser(body){
-        return  fetch(`https://course-django-react-api-production.up.railway.app/auth/`,{
-             method:'POST',
-             headers:{
-               'Content-Type': 'application/json'
-             },
-             body:JSON.stringify(body)
-           }).then(apires=>apires.json())
-         
-     }
-     static registerUser(body){
-      return  fetch(`https://course-django-react-api-production.up.railway.app/api/users/`,{
-           method:'POST',
-           headers:{
-             'Content-Type': 'application/json'
-           },
-           body:JSON.stringify(body)
-         }).then(apires=>apires.json())
-       
-   }
-
-    static updateMovie(mov_id ,body,token){
-       return  fetch(`https://course-django-react-api-production.up.railway.app/api/movies/${mov_id}/`,{
-            method:'PUT',
-            headers:{
-              'Content-Type': 'application/json',
-              'Authorization':`Token ${token}`
-            },
-            body:JSON.stringify(body)
-          }).then(apires=>apires.json())
-        
-    }
-
-    static createMovie(body,token){
-        return  fetch(`https://course-django-react-api-production.up.railway.app/api/movies/`,{
-             method:'Post',
-             headers:{
-               'Content-Type': 'application/json',
-               'Authorization':`Token ${token}`
-             },
-             body:JSON.stringify(body)
-           }).then(apires=>apires.json())
-         
-     }
-     static getMovies(token){
-
-     return fetch(`https://course-django-react-api-production.up.railway.app/api/movies/`,{
-        method:'GET',
-        headers:{
-          'Content-Type': 'application/json',
-          'Authorization':`Token ${token}`
-        }
-      })
-      .then(apires=>apires.json())
-
-
-     }
-
-     static deleteMovie(mov_id, token) {
-      return fetch(`https://course-django-react-api-production.up.railway.app/api/movies/${mov_id}/`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Token ${token}`
-        }
-      })
-    }
-
+  static getMovies(token){
+    return fetch("https://course-django-react-api-production.up.railway.app/api/movies/", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${token}` 
+      }
+    }).then( resp => resp.json())
+  }
+  static updateMovie(mov_id, body, token) {
+    return fetch(`https://course-django-react-api-production.up.railway.app/api/movies/${mov_id}/`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${token}`
+      },
+      body: JSON.stringify( body )
+    }).then( resp => resp.json())
+  }
+  static createMovie(body, token) {
+    return fetch(`https://course-django-react-api-production.up.railway.app/api/movies/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${token}`
+      },
+      body: JSON.stringify( body )
+    }).then( resp => resp.json())
+  }
+  static deleteMovie(mov_id, token) {
+    return fetch(`https://course-django-react-api-production.up.railway.app/api/movies/${mov_id}/`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${token}`
+      }
+    })
+  }
 }
--- src/components/auth.js
+++ src/components/auth.js
@@ -1,36 +1,33 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { API } from "../api-service";
-import { useCookies } from "react-cookie";
+import React, { useState, useEffect } from 'react';
+import { API } from '../api-service';
+import { useCookies } from 'react-cookie';
 
 function Auth(){
-    const [ username, setUsername ] = useState('');
-    const [ password, setPassword ] = useState('');
-    const [ isLoginView, setIsLoginView ] = useState(true);
-    const [token, setToken] = useCookies(['mr-token']);
 
-    useEffect(()=>{
-        console.log(token)
-       if(token['mr-token']) window.location.href = '/movies';
-    },[token])
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ isLoginView, setIsLoginView ] = useState(true);
 
-    const loginClicked = () => {
-        API.loginUser({username, password})
-        .then( resp => setToken('mr-token',resp.token))
-        .then(res=>console.log(res))
-          .catch( error => console.log(error))
-      }
+  const [token, setToken] = useCookies(['mr-token']);
 
-      const registerClicked = () => {
-        API.registerUser({username, password})
-          .then( () => loginClicked())
-          // .then(res=>console.log(res))
-          .catch( error => console.log(error))
-      }
-      const isDisabled = username.length === 0 || password.length === 0;
+  useEffect( () => {
+    if(token['mr-token']) window.location.href = '/movies';
+  }, [token])
 
-    return(
-        <div className="App">
+  const loginClicked = () => {
+    API.loginUser({username, password})
+      .then( resp => setToken('mr-token', resp.token))
+      .catch( error => console.log(error))
+  }
+  const registerClicked = () => {
+    API.registerUser({username, password})
+      .then( () => loginClicked())
+      .catch( error => console.log(error))
+  }
+  const isDisabled = username.length === 0 || password.length === 0;
+
+  return (
+    <div className="App">
       <header className="App-header">
         {isLoginView ? <h1>Login</h1> : <h1>Register</h1>}
       </header>
@@ -40,10 +37,8 @@ function Auth(){
               onChange={ evt=> setUsername(evt.target.value)}
         /><br/>
         <label htmlFor="password">Password</label><br/>
-        <input id="password" type="password" placeholder="password" value={password}
+        <input id="password" type="password" placeholder="Descriptiom" value={password}
             onChange={ evt=> setPassword(evt.target.value)} /><br/>
-
-
             { isLoginView ?
               <button onClick={loginClicked} disabled={isDisabled}>Login</button> : 
               <button onClick={registerClicked} disabled={isDisabled}>Register</button>
@@ -55,7 +50,7 @@ function Auth(){
         }
       </div>
     </div>
-    )
+  )
 }
 
 export default Auth;
