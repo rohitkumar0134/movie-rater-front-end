5cc9f0b50ad1bc1dd52d945ca20b3076c9ed8c2b
rohitkumar0134 <rohit54pandit@gmail.com>
2023-03-16 12:24:53 +0530

    Updates

---
+++ precache-manifest.6330f2e213b5e509fcb37c52c283bf2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4092350c1a74b02275ee",
+    "url": "/movie-rater-front-end/static/css/main.6a65e9a7.chunk.css"
+  },
+  {
+    "revision": "4092350c1a74b02275ee",
+    "url": "/movie-rater-front-end/static/js/main.4092350c.chunk.js"
+  },
+  {
+    "revision": "ff3e70574e23ec862a62",
+    "url": "/movie-rater-front-end/static/js/1.ff3e7057.chunk.js"
+  },
+  {
+    "revision": "766b981e5a19fc78ba8a",
+    "url": "/movie-rater-front-end/static/js/2.766b981e.chunk.js"
+  },
+  {
+    "revision": "46c5d292b0b542646581",
+    "url": "/movie-rater-front-end/static/js/runtime~main.46c5d292.js"
+  },
+  {
+    "revision": "8977d330e1802d88f85606d770319b27",
+    "url": "/movie-rater-front-end/index.html"
+  }
+];
